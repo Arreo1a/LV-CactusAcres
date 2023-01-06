512483d837fb33ddf4fcb8e4e85bffb65118a73d
ray <servitouch7@gmail.com>
2023-01-06 10:37:02 -0700

    terreno

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ import HomePage from "./pages/lebaronVecindad/pages/HomePage/HomePage";
 import CactusAcresHomePage from "./pages/cactusAcres/pages/HomePage/CactusAcresHomePage";
 import RayosDeSolNorteHomePage from "./pages/rayosDeSolNorte/pages/HomePage/RayosDeSolNorteHomePage";
 import PDSHomePage from "./pages/PuestaDeSol/pages/HomePage/PDSHomePage";
+import HomePageFrancisLeany from "./pages/terrenoFrancisLeany/HomePage";
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} exact />
+       <Route path="/" element={<HomePage />} exact />
         <Route path="/cactus-acres" element={<CactusAcresHomePage />} />
+        <Route path="/terreno-francis-leany" element={<HomePageFrancisLeany />} />
         <Route
           path="/rayos-de-sol-norte"
           element={<RayosDeSolNorteHomePage />}
         />
         <Route path="/puesta-de-sol" element={<PDSHomePage />} />
+
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
